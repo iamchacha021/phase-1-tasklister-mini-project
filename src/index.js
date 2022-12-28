548cc62d43cc468a40f9faa3caed93e1a21109d4
iamchacha021 <iamchacha021@gmail.com>
2022-12-28 14:29:20 +0300

    completed minitask

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const ul = document.getElementById('tasks')
+  const form = document.forms["create-task-form"]
+  form.addEventListener('submit', (ev)=>{
+    ev.preventDefault()
+    const value = form.querySelector('input[type="text"]').value
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+    const deleteBtn = document.createElement('button')
+    deleteBtn.addEventListener('click',(ev)=>{
+      ev.target.parentElement.remove()
+    })
+
+    li.appendChild(p)
+    li.appendChild(deleteBtn)
+    ul.appendChild(li)
+
+    // li.textContent = value
+    p.textContent = value
+    deleteBtn.textContent = 'Remove Task'
+  })
 });
